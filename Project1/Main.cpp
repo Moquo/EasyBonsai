388478e18fdaf418f810794ea9cf46492656afc4
Git-Curve <fynnbwdt@gmail.com>
2018-09-06 17:29:20 +0200

    Projektdateien hinzufügen.

---
+++ Project1/Main.cpp
@@ -0,0 +1,77 @@
+﻿#include "Usefulstuff.hpp"
+
+/*
+
+Easy Bonsai Assembler
+
+	Makes programming in (Bonsai)Assembler easier by translating a easier version of BonsaiAssembler to normal BonsaiAssembler.
+
+	Libs used:
+	https://archive.codeplex.com/?p=cppconlib -- Console Utility
+
+*/
+int main(int argc, char* args[]) {
+
+	console_out_context ctxout;
+	console_out conout(ctxout);
+
+	conout.settitle("EasyBonsai ¯\_(ツ)_/¯");
+
+	string filename;
+	if (argc < 2) {
+		log("Input Filename: ", console_text_colors::light_blue, console_bg_colors::white, false);
+		cin >> settextcolor(console_text_colors::white)
+			>> setbgcolor(console_bg_colors::black)
+			>> filename;
+	}
+	else {
+		filename = std::string(args[1]);
+	}
+
+	struct stat info;
+
+	if (stat(filename.c_str(), &info) == 0) {
+		log("Reading File: " + filename, console_text_colors::light_green);
+
+		ifstream in(filename.c_str());
+		string buff;
+		vector<string> input;
+		while (getline(in, buff)) {
+			input.push_back(buff);
+		}
+		log("Invalid Instructions will be ignored, so you can freely comment your code!", console_text_colors::light_yellow);
+		log("Reading File... Translating Code: ", console_text_colors::light_cyan);
+		cout << endl;
+
+		vector<string> builtcode;
+
+		for (auto x : input) {
+			if (isValid(x)) {
+				builtcode.push_back(x); 
+				cout << setbgcolor(console_bg_colors::white) << settextcolor(console_text_colors::magenta) << x << endl;
+				ctxout.restore(console_cleanup_options::restore_attibutes);
+			}
+			else {
+				cout << "(INVALID)" << settextcolor(console_text_colors::light_red) << x << endl;
+				ctxout.restore(console_cleanup_options::restore_attibutes);
+			}
+		}
+
+		cout << endl << endl;
+
+		log("Parsed Code: \n", console_text_colors::light_green);
+		cout << setbgcolor(console_bg_colors::white) << settextcolor(console_text_colors::magenta) << join(builtcode, '\n') << endl << endl;
+		ctxout.restore(console_cleanup_options::restore_attibutes);
+
+		log("Translating code...", console_text_colors::light_yellow);
+
+
+	}
+	else {
+		log("Could not find file or directory: " + filename, console_text_colors::light_red, console_bg_colors::white);
+	}
+
+	system("pause");
+
+	return 0;
+}
---
+++ Project1/Usefulstuff.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <iostream>
+#include "cppconlib.h"
+#include <sys/stat.h>
+#include <fstream>
+#include <sys/types.h>
+#include <regex>
+
+using namespace std;
+using namespace conmanip;
+void log(string message, console_text_colors color = console_text_colors::white, console_bg_colors bgcolor = console_bg_colors::black, bool endline = true);
+void log(string message, console_text_colors color, console_bg_colors bgcolor, bool endline) {
+
+	cout
+		<< settextcolor(color)
+		<< setbgcolor(bgcolor)
+		<< "[EasyBonsai] >> " << message
+		<< settextcolor(console_text_colors::white)
+		<< setbgcolor(console_bg_colors::black)
+		<< " " << ((endline == true) ? "\n" : "");
+
+}
+std::vector<std::string> split(std::string str, std::string sep);
+std::vector<std::string> split(std::string str, std::string sep) {
+	char* cstr = const_cast<char*>(str.c_str());
+	char* current;
+	std::vector<std::string> arr;
+	current = strtok(cstr, sep.c_str());
+	while (current != NULL) {
+		arr.push_back(current);
+		current = strtok(NULL, sep.c_str());
+	}
+	return arr;
+}
+
+regex validinst( R"rgx(^(jmp \d)|(tst \d)|(dec \d)|(inc \d)$)rgx" );
+bool isValid(string input);
+bool isValid(string input) {
+	if (regex_match(input, validinst)) {
+		return true;
+	}
+	return false;
+}
+
+string join(const vector<string>& v, char c);
+string join(const vector<string>& v, char c) {
+
+	string s;
+
+	s.clear();
+
+	for (vector<string>::const_iterator p = v.begin();
+		p != v.end(); ++p) {
+		s += *p;
+		if (p != v.end() - 1)
+			s += c;
+	}
+
+	return s;
+
+}
