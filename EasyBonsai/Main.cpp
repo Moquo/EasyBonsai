6be0164d47c5ec087741cd2a4405a0ed97aec608
Git-Curve <fynnbwdt@gmail.com>
2018-09-08 00:25:10 +0200

    First working? release :)

--- EasyBonsai/Main.cpp
+++ EasyBonsai/Main.cpp
@@ -13,13 +13,13 @@ Easy Bonsai Assembler
 int main(int argc, char* args[])
 {
 	console_out_context ctxout;
-	console_out conout(ctxout);
+	console_out conout(ctxout); //Some Console Utilities...
 
-	if (argc < 2 || argc > 2) {
+	if (argc < 2 || argc > 2) { //Check if there is exactly one start argument (file).
 		log("Argument Error", console_text_colors::light_red, console_bg_colors::black, false, "Error");
 		return 0;
 	}
-	if ((string)args[1] == "help")
+	if ((string)args[1] == "help") //Help 'n Stuff :)
 	{
 		cout
 			<< "EasyBonsai is a easier version of the BonsaiAssembler which will be converted back to runnable (Bonsai)Assembler." << endl
@@ -28,53 +28,51 @@ int main(int argc, char* args[])
 			<< "- it supports jmp with skipping, (so jmp +2 will jump 2 lines forward)" << endl;
 		return 0;
 	}
-	struct stat info;
+	struct stat info; //Needed for detecting wether file exists or not.
 	if (stat(args[1], &info) == 0) {
-		//Todo: Add Error Message if krebs
 		ifstream in(args[1]);
 		string buff;
 		vector<string> input;
 		while (getline(in, buff)) {
 			input.push_back(buff);
-		}
+		} //Read File line by line.
 		log("Read File: " + (string)args[1], console_text_colors::light_green, console_bg_colors::black, true, "Success");
-		//Todo end.
 		log("Invalid Instructions will be ignored, so you can freely comment your code!", console_text_colors::light_yellow, console_bg_colors::black, true, "Info");
 		log("Translating Code", console_text_colors::light_white, console_bg_colors::black, true, "Started");
-		cout << endl;
+		cout << endl; //Tell user we read file.
 
-		vector<string> builtcode;
-		for (auto x : input) {
-			if (isValid(x)) {
+		vector<string> builtcode; //Array which will hold the code which we will later translate.
+		for (auto x : input) { // We will output the code here, so the user knows which lines we will use or not use.
+			if (isValid(x)) { //Check if Line is valid/allowed instruction.
 				builtcode.push_back(x);
 				cout << setbgcolor(console_bg_colors::white) << settextcolor(console_text_colors::black) << x << endl;
 				ctxout.restore(console_cleanup_options::restore_attibutes);
 			}
 			else
 			{
-				if (strncmp(x.c_str(), "//", 2) == 0)
+				if (strncmp(x.c_str(), "//", 2) == 0) //If Line starts with // we wont spam the user with red error messages and just ignore it.
 					cout << settextcolor(console_text_colors::light_white) << "// " << settextcolor(console_text_colors::white) << x.substr(2) << endl;
 				else
 					cout << settextcolor(console_text_colors::light_red) << "(INVALID) " << settextcolor(console_text_colors::white) << x << endl;
 
-				ctxout.restore(console_cleanup_options::restore_attibutes);
+				ctxout.restore(console_cleanup_options::restore_attibutes); //This will restore colors.
 			}
 		}
 		cout << endl;
-		
+		/*
+		Basically this is fucking useless.
 		log("Parsed Code: \n", console_text_colors::light_green, console_bg_colors::black, true, "Success");
 		for (int i = 0; builtcode.size() > i; i++) {
 			cout << setbgcolor(console_bg_colors::white) << settextcolor(console_text_colors::black) << i << ": " << builtcode[i] << endl;
 		}
 		ctxout.restore(console_cleanup_options::restore_attibutes);
 		cout << endl;
-
-		vector<string> bonsaicode = translate(builtcode);
+		*/
+		vector<string> bonsaicode = translate(builtcode); //Translate the Code and output it.
 		log("Bonsai: ", console_text_colors::light_green, console_bg_colors::black, true, "Success");
 		cout << endl << setbgcolor(console_bg_colors::white) << settextcolor(console_text_colors::black) << join(bonsaicode, '\n') << endl;
 		ctxout.restore(console_cleanup_options::restore_attibutes);
 		cout << endl;
-
 	}
 	else
 	{
@@ -84,63 +82,78 @@ int main(int argc, char* args[])
 }
 
 void format(vector<string> *in) {
-	for (int i = 0; (*in).size() > i; i++) {
-		if (isMarked((*in)[i]))
-			(*in)[i] = (*in)[i].substr(3);
-		if (!isFormatted(((*in)[i])))
+	for (int i = 0; (*in).size() > i; i++) { //Loop through the code lines
+		if (isMarked((*in)[i])) //If Line starts with "x: " then remove the beginning because this is no longer needed.
+			(*in)[i] = (*in)[i].substr(3); // This skips "x: "
+		if (!isFormatted(((*in)[i]))) //If Line is not formatted, format it... (Include Line Numbers)
 			(*in)[i] = std::string(::to_string(i) + ": " + (*in)[i]);
 	}
 }
 void JmpOverwrite(vector<string> *in) {
 	for (int i = 0; (*in).size() > i; i++) {
-		if (isEasyJmp((*in)[i])) {
-			if (strstr((*in)[i].c_str(), "-")) {
-				string afterminus = split(string((*in)[i]), "-")[1];
-				int parsedskip = std::atoi(afterminus.c_str());
-				(*in)[i] = std::string(::to_string(i) + ": jmp " + ::to_string(i - parsedskip));
+		if (isEasyJmp((*in)[i])) { //If Jmp goes something like this: jmp +2 then redirect it to the right line.
+			if (strstr((*in)[i].c_str(), "-")) {  //Jmp back
+				string afterminus = split(string((*in)[i]), "-")[1]; //Get the Amount of lines we want to skip
+				int parsedskip = std::atoi(afterminus.c_str()); //Convert the Amount to an int.
+				(*in)[i] = std::string(::to_string(i) + ": jmp " + ::to_string(i - parsedskip)); //Replace the instruction
 			}
-			if (strstr((*in)[i].c_str(), "+")) {
-				string afterplus = split(string((*in)[i]), "+")[1];
-				int parsedskip = std::atoi(afterplus.c_str());
-				(*in)[i] = std::string(::to_string(i) + ": jmp " + ::to_string(i + parsedskip));
+			if (strstr((*in)[i].c_str(), "+")) { //Jmp forward
+				string afterplus = split(string((*in)[i]), "+")[1]; //Get the Amount of lines we want to skip
+				int parsedskip = std::atoi(afterplus.c_str()); //Convert the Amount to an int.
+				(*in)[i] = std::string(::to_string(i) + ": jmp " + ::to_string(i + parsedskip)); //Replace the instruction
 			}
 		}
 	}
 }
 void FunctionSystem(vector<string> *in) {
-	vector<pair<int,string>> markedFuncs;
+	vector<pair<int, string>> markedFuncs;
 	for (int i = 0; (*in).size() > i; i++) {
-		if (isMarked((*in)[i])) {
-			string funcName = split((*in)[i], ":")[0];
-			markedFuncs.push_back(std::make_pair<int,string>((int)i, (string)funcName));
+		if (isMarked((*in)[i])) { //If Instruction is marked as function beginning (x: (inst) (arg))
+			string funcName = (*in)[i].substr(0, 1); //Get Name of Function.
+			markedFuncs.push_back(make_pair(i, funcName)); //Add the found Function to the FuncList.
 		}
 	}
-	for (auto x : markedFuncs) {
-		string funcName = x.second;
+	for (pair<int, string> x : markedFuncs) {
 		int funcLine = x.first;
+		string funcName = x.second;
 		for (int i = 0; (*in).size() > i; i++) {
-			if (isFuncJmp((*in)[i])) {
-				string _fName = (*in)[i].substr((*in)[i].size() - 1);
-				if (_fName.substr(_fName.size()-1) == funcName) {
-					(*in)[i] = string((*in)[i].substr(0, ((*in)[i].find(_fName.c_str()))) + ::to_string(funcLine));
+			if (isFuncJmp((*in)[i])) { //If instruction refers func.
+				string _fName = (*in)[i].substr((*in)[i].size() - 1); //Get refered function name.
+				if (_fName == funcName) { //if function name equals current func Name
+					(*in)[i] = string((*in)[i].substr(0, ((*in)[i].find(_fName.c_str()))) + ::to_string(funcLine)); //Replace line with refer to correct line.
 				}
-				markedFuncs.push_back(std::make_pair<int, string>((int)i, (string)funcName));
 			}
+		}
+	}
+}
+void Allocation(vector<string> *in) {
+	for (int i = 0; (*in).size() > i; i++) {
+		if (isZeroAllocation((*in)[i])) {
+			string first = split((*in)[i], "[")[0].substr(0, 1);
+			vector<vector<string>> func = getZeroAllocation(i, (*in).size(), first);
+			(*in)[i] = func[0][0];
+			for (auto x : func[1])
+				(*in).push_back(x);
+		}
+		if (isNormalAllocation((*in)[i])) {
+			string first = split((*in)[i], "[")[0].substr(0, 1);
+			string second = split((*in)[i], "[")[0].substr(2, 1);
+			string help = split((*in)[i], "[")[0].substr(4, 1);
+			vector<vector<string>> func = getNormalAllocation(i, (*in).size(), first, second, help);
+			(*in)[i] = func[0][0];
+			for (auto x : func[1])
+				(*in).push_back(x);
 		}
 	}
 }
 
 vector<string> translate(vector<string> inp) {
-
 	vector<string> t_code = inp;
 
-	//Create Pre-Function Instructions
-	//Overwrite JMPS to be correct.
-	FunctionSystem(&t_code);
-	JmpOverwrite(&t_code);
-	//Append Functions
-	//Format Code
-	format(&t_code);
+	FunctionSystem(&t_code); //Redirect Function References to correct lines.
+	JmpOverwrite(&t_code); //Convert (5: JMP -2) to (5: JMP 3) and so on :)
+	Allocation(&t_code); //Append Functions
+	format(&t_code); //Get Code in right format.
 
 	return t_code;
 }
--- EasyBonsai/Usefulstuff.hpp
+++ EasyBonsai/Usefulstuff.hpp
@@ -18,20 +18,24 @@ bool isMarked(string input);
 bool isEasyJmp(string input);
 bool isFuncJmp(string input);
 bool isFormatted(string input);
+bool isZeroAllocation(string input);
+bool isNormalAllocation(string input);
 
 vector<string> translate(vector<string> inp);
 vector<std::string> split(std::string str, std::string sep);
+vector<vector<string>> getZeroAllocation(int back, int line, string first);
+vector<vector<string>> getNormalAllocation(int back, int line/*(*in).size()*/, string first, string second, string help);
 
 string join(const vector<string>& v, char c);
 
 void format(vector<string> *in);
 void JmpOverwrite(vector<string> *in);
 void FunctionSystem(vector<string> *in);
-void log(string message, console_text_colors color = console_text_colors::white, console_bg_colors bgcolor = console_bg_colors::black, bool endline = true,string prefix = "");
+void log(string message, console_text_colors color = console_text_colors::white, console_bg_colors bgcolor = console_bg_colors::black, bool endline = true, string prefix = "");
 
-
-//regex validinst(R"rgx(^(.?:? ?)((jmp .?\d)|(jmp [a-z])|(tst \d)|(dec \d)|(inc \d)|(hlt))$)rgx");
-regex validinst(R"rgx(^(.?:? ?)((jmp [+-]?\d)|(jmp [a-z])|(tst \d)|(dec \d)|(inc \d)|(hlt))$)rgx");
+//Basic Regex for instruction recognition.
+//regex validinst(R"rgx(^(.?:? ?)((jmp [+-]?\d+)|(jmp [a-z])|(tst \d)|(dec \d)|(inc \d)|(hlt)|([[]\d[=](zero)[]]))$)rgx");
+regex validinst(R"rgx(^(.?:? ?)((jmp [+-]?\d+)|(jmp [a-z])|(tst \d)|(dec \d)|(inc \d)|(hlt)|(\[\d=zero\])|(\[\d=\d\#\d\]))$)rgx");
 bool isValid(string input) {
 	if (regex_match(input, validinst)) {
 		return true;
@@ -39,7 +43,7 @@ bool isValid(string input) {
 	return false;
 }
 
-regex easyjmp(R"rgx(^(jmp \+\d)|(jmp \-\d)$)rgx");
+regex easyjmp(R"rgx(^(jmp \+\d+)|(jmp \-\d+)$)rgx");
 bool isEasyJmp(string input) {
 	if (regex_match(input, easyjmp)) {
 		return true;
@@ -71,17 +75,89 @@ bool isFuncJmp(string input) {
 	return false;
 }
 
+regex zallocation(R"rgx(^(\[\d=zero\])$)rgx");
+bool isZeroAllocation(string input) {
+	if (regex_match(input, zallocation)) {
+		return true;
+	}
+	return false;
+}
+
+regex normalallocation(R"rgx(^(\[\d=\d\#\d\])$)rgx");
+bool isNormalAllocation(string input) {
+	if (regex_match(input, normalallocation)) {
+		return true;
+	}
+	return false;
+}
+
 void log(string message, console_text_colors color, console_bg_colors bgcolor, bool endline, string prefix) {
 	cout
 		<< settextcolor(color)
 		<< setbgcolor(bgcolor)
-		<< (prefix != "" ? "["+prefix+"] " : "")
+		<< (prefix != "" ? "[" + prefix + "] " : "")
 		<< settextcolor(console_text_colors::white)
 		<< setbgcolor(console_bg_colors::black)
 		<< message
 		<< " " << ((endline == true) ? "\n" : "");
 }
 
+
+vector<vector<string>> getZeroAllocation(int back, int line/*(*in).size()*/, string first) {
+	vector<vector<string>> rtn;
+	string sline = ::to_string(line);
+	string sback = ::to_string(back);
+	rtn.push_back({ string("jmp " + sline) });
+
+	vector<string> func;
+	func.push_back("tst " + first); //Line Number of this is sLine
+	func.push_back("jmp " + ::to_string(line + 3)); //Jump to dec;
+	func.push_back("jmp " + ::to_string(back + 1)); //Jump back to initial line + 1 cuz we done with that
+	func.push_back("dec " + first);
+	func.push_back("jmp " + sline);
+
+	rtn.push_back(func);
+
+	return rtn;
+}
+vector<vector<string>> getNormalAllocation(int back, int line/*(*in).size()*/, string first, string second, string help) {
+	vector<vector<string>> rtn;
+	string sline = ::to_string(line);
+	string sback = ::to_string(back);
+	rtn.push_back({ string("jmp " + sline) });
+
+	vector<string> func;
+	//Im not even going to try to commentate this shit properly...
+	/*0*/func.push_back("tst " + second); //TST Y
+	/*1*/func.push_back("jmp " + ::to_string(line + 3)); //JMP +2
+	/*2*/func.push_back("jmp " + ::to_string(line + 5)); //JMP +3
+	/*3*/func.push_back("dec " + second); //DEC Y
+	/*4*/func.push_back("jmp " + sline); //JMP 0
+	/*5*/func.push_back("tst " + help); //TST H
+	/*6*/func.push_back("jmp " + ::to_string(line + 8)); //JMP +2
+	/*7*/func.push_back("jmp " + ::to_string(line + 10)); //JMP +3
+	/*8*/func.push_back("dec " + help); //DEC H
+	/*9*/func.push_back("jmp " + ::to_string(line + 5)); //JMP -3
+	/*10*/func.push_back("tst " + first); //TST X
+	/*11*/func.push_back("jmp " + ::to_string(line + 13)); //JMP +2
+	/*12*/func.push_back("jmp " + ::to_string(line + 17)); //JMP +5
+	/*13*/func.push_back("dec " + first); //DEC X
+	/*14*/func.push_back("inc " + second); // INC Y
+	/*15*/func.push_back("inc " + help); // INC H
+	/*16*/func.push_back("jmp " + ::to_string(line+10)); // JMP -6 (BEGINNING)
+	/*17*/func.push_back("tst " + help); // TST H
+	/*18*/func.push_back("jmp " + ::to_string(line + 20)); // JMP +2
+	/*19*/func.push_back("jmp " + ::to_string(back + 1)); // JMP +4 (OUTSIDE OF FUNCTION)
+	/*20*/func.push_back("dec " + help); // DEC H
+	/*21*/func.push_back("inc " + first); // INC X
+	/*22*/func.push_back("jmp " + ::to_string(line + 17)); // JMP -5
+
+	rtn.push_back(func);
+
+	return rtn;
+}
+
+//Stole those from Stackoverflow :^)
 std::vector<std::string> split(std::string str, std::string sep) {
 	char* cstr = const_cast<char*>(str.c_str());
 	char* current;
